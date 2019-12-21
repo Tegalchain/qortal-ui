148dfa61907a2b016be2c43ee4c43baa1b4233d2
irontiga <irontiga@gmail.com>
2019-12-21 13:24:02 +1300

    no message

--- config/build.config.js
+++ config/build.config.js
@@ -7,6 +7,10 @@ const build = {
         ...defaultConfig.build.options,
         outputDir: path.join(__dirname, '../build'),
         imgDir: path.join(__dirname, '../img')
+    },
+    aliases: {
+        '@frag/crypto': path.join(__dirname, '../node_modules/frag-qortal-crypto/api.js')
+        // '@frag/crypto': path.join(__dirname, '../node_modules/frag-qora-crypto/')
     }
 }
 

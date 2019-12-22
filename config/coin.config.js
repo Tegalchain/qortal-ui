3b1001d6e5cc3f7c00a52006debc2959316dd18a
irontiga <irontiga@gmail.com>
2019-12-23 06:03:54 +1300

    assets

--- config/coin.config.js
+++ config/coin.config.js
@@ -2,5 +2,8 @@ const defaultConfig = require('./default.config.js')
 
 module.exports = {
     ...defaultConfig.coin,
+    name: 'Qortal',
+    symbol: 'Qort',
+    addressVersion: 58, // Q for Qortal
     logo: '/img/QORT_LOGO.svg'
 }

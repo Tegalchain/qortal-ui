bd1e83df3fff1d94941849579cd60daa1b7ad914
irontiga <irontiga@gmail.com>
2020-02-21 00:37:04 +1300

    no message

--- config/user.config.js
+++ config/user.config.js
@@ -1,9 +1,31 @@
 const user = require('./default.config.js').user
 
 module.exports = {
-    node: {
-        protocol: 'http',
-        domain: '127.0.0.1',
-        port: 12391,
-    }
+    node: 0
+    // node: {
+    //     protocol: 'http',
+    //     domain: '127.0.0.1',
+    //     port: 12391,
+    // },
+    // server: {
+    //     writeHosts: {
+    //         enabled: false
+    //     },
+    //     primary: {
+    //         domain: 'qor.tal',
+    //         address: '127.0.0.1', // What the domain should point to
+    //         port: 80, // Port to access the Qora UI from
+    //         directory: './src/', // Core Qora-lite code.,
+    //         page404: './src/404.html',
+    //         host: '0.0.0.0' // This probably shouldn't be the default...
+    //     },
+    //     plugin: {
+    //         domain: 'qor.tal', // '*.domain' is used to host subdomains
+    //         address: '127.0.0.1',
+    //         port: 80, // meh, why not keep it, who knows what kind of stuff people get into
+    //         directory: './plugins', // Where the plugin folders are stored,
+    //         default: 'wallet',
+    //         host: '0.0.0.0' // frag.ui?
+    //     }
+    // }
 }

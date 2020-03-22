61f8168224a2a81a4e03d6ad636d9a95fa3f478e
LOTW <lotw7270@protonmail.com>
2020-03-22 03:57:12 +0100

    Fixes for build

--- builtWWW/es6/components/show-plugin.js
+++
@@ -1,7 +0,0 @@
-import '../default-theme-95ad2c42.js';
-import '../pwa-helpers-4ce9032e.js';
-import '../typography-9c029054.js';
-import '../iron-a11y-keys-behavior-491299bd.js';
-import '../show-plugin-cfab0b6a.js';
-import '../iron-overlay-behavior-7b397a72.js';
-//# sourceMappingURL=show-plugin.js.map

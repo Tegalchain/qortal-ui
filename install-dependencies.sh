d164dd7ed7249744440305150c640cc578e28b21
LOTW <lotw7270@protonmail.com>
2020-03-25 02:22:47 +0100

    New updates to Fix Windows Blank Screen

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -14,41 +14,41 @@ install_dependencies()
   echo -e '---CLONING AND INSTALLING ALL UI REPOSITORIES---'
 
   echo -e '---CLONING UI CORE---'
-  # cd ../
+  cd ../
   git clone https://github.com/Qortal/qortal-ui-core.git
   cd qortal-ui-core
   echo -e '---INSTALLING UI CORE---'
   yarn install
-  # cd ../
-  # cp -R qortal-ui-core qortal-ui/qortal-ui-core
-  # cd qortal-ui/qortal-ui-core\
+  cd ../
+  cp -R qortal-ui-core qortal-ui/qortal-ui-core
+  cd qortal-ui/qortal-ui-core
   echo -e '---CREATING UI CORE LINK---'
   yarn link
-  # cd ../../
-  cd ../
+  cd ../../
+  # cd ../
 
   echo -e '---CLONING UI PLUGINS---'
   git clone https://github.com/Qortal/qortal-ui-plugins.git
   cd qortal-ui-plugins
   echo -e '---INSTALLING UI PLUGINS---'
   yarn install
-  # cd ../
-  # cp -R qortal-ui-plugins qortal-ui/qortal-ui-plugins
-  # cd qortal-ui/qortal-ui-plugins
+  cd ../
+  cp -R qortal-ui-plugins qortal-ui/qortal-ui-plugins
+  cd qortal-ui/qortal-ui-plugins
   echo -e '---CREATING UI PLUGINS LINK---'
   yarn link
-  # cd ../../
-  cd ../
+  cd ../../
+  # cd ../
 
   echo -e '---CLONING UI CRYPTO---'
   git clone https://github.com/Qortal/qortal-ui-crypto.git
   cd qortal-ui-crypto
   echo -e '---INSTALLING UI CRYPTO---'
   yarn install
-  # cd ../
-  # cp -R qortal-ui-crypto qortal-ui/qortal-ui-crypto
-  # cd qortal-ui/qortal-ui-crypto
-  echo -e '---CREATING UI CRYPTO---'
+  cd ../
+  cp -R qortal-ui-crypto qortal-ui/qortal-ui-crypto
+  cd qortal-ui/qortal-ui-crypto
+  echo -e '---CREATING UI CRYPTO LINK---'
   yarn link
   cd ../
  

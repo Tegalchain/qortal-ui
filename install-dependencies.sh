586dbca4088778422ab853003ee6467d7ea572d4
LOTW <lotw7270@protonmail.com>
2020-07-26 03:30:24 +0100

    FIX: fixed install dependency error

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -65,14 +65,14 @@ install_dependencies()
   # yarn remove @material/layout-grid @material/mwc-button @material/mwc-checkbox @material/mwc-dialog @material/mwc-drawer @material/mwc-fab @material/mwc-formfield @material/mwc-icon @material/mwc-icon-button @material/mwc-list @material/mwc-select @material/mwc-snackbar @material/mwc-textfield @material/mwc-top-app-bar
   cd ../
   cd qortal-ui-plugins
-  yarn install --production --ignore-scripts --prefer-offline
-  rm node_modules
+  # yarn install --production --ignore-scripts --prefer-offline
+  rm -R node_modules
   # yarn remove @github/time-elements @material/mwc-button @material/mwc-dialog @material/mwc-icon @material/mwc-slider @material/mwc-textfield @polymer/paper-input @polymer/paper-progress @polymer/paper-spinner @vaadin/vaadin-grid @webcomponents/webcomponentsjs lit-html
   # cd ../
   cd ../
   cd qortal-ui-crypto
-  yarn install --production --ignore-scripts --prefer-offline
-  rm node_modules
+  # yarn install --production --ignore-scripts --prefer-offline
+  rm -R node_modules
   cd ../
   rm -R qortal-ui-crypto
 

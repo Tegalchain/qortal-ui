ab5060b74de4ed689a4bd9993dd17e8bdc4a41ac
LOTW <lotw7270@protonmail.com>
2020-08-19 12:03:18 +0100

    FIX: fix installation errors on Debian

--- scripts/add-debian-apt-repo.sh
+++ scripts/add-debian-apt-repo.sh
@@ -6,10 +6,15 @@
 ln -sf '/opt/${productFilename}/${executable}' '/usr/bin/${executable}'
 
 # SUID chrome-sandbox for Electron 5+
-chmod 4755 '/opt/${productFilename}/chrome-sandbox' || true
+sudo chown root '/opt/${productFilename}/chrome-sandbox' || true
+sudo chmod 4755 '/opt/${productFilename}/chrome-sandbox' || true
+sudo sysctl kernel.unprivileged_userns_clone=1
 
 update-mime-database /usr/share/mime || true
 update-desktop-database /usr/share/applications || true
+
+# Install curl if not installed on the system
+if ! dpkg-query -W -f='${Status}' curl | grep "ok installed"; then sudo apt-get --yes install curl; fi
 
 # Install apt repository source list if it does not exist
 if ! grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep qortal.list; then

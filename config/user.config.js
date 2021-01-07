0682c725b08dc322b5394f3e7c456ba02b429a81
LOTW <lotw7270@protonmail.com>
2021-01-07 11:29:50 +0100

    Refactor: change to mainnet from testnet

--- config/user.config.js
+++ config/user.config.js
@@ -1,10 +1,10 @@
 const user = require('./default.config.js').user
 module.exports = {
-    node: 1, // set to mainnet
-    server: {
-        primary: {
-            port: 12388,
-            address: '0.0.0.0'
-        }
-    }
+	node: 1, // set to mainnet
+	server: {
+		primary: {
+			port: 12388,
+			address: '0.0.0.0',
+		},
+	},
 }
